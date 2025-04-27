a102e194e291e9af0a35128c59427e8ba26b3452
Alekres <alex072006@mail.ru>
2025-04-27 11:19:42 +0300

    27.04.25_v-0.0.1_fix_api_deepseek

--- deepseek.py
+++ deepseek.py
@@ -1,13 +1,14 @@
 from dsk.api import DeepSeekAPI
+from api_key import AI_api_token
 
 # Инициализация с вашим токеном авторизации
-api = DeepSeekAPI("Z6QZ+1OTzZIp2XqZDs4qUN+tukCZwy2u5PhLzRuZJE+5DNHLRaF5FwOjTr3yF5Kk")
+api = DeepSeekAPI(AI_api_token)
 
 # Создание новой чат-сессии
 chat_id = api.create_chat_session()
 
 # Простой чат-комплешн
-prompt = "Что такое Python?"
+prompt = "Привет"
 for chunk in api.chat_completion(chat_id, prompt):
     if chunk['type'] == 'text':
         print(chunk['content'], end='', flush=True)
--- yandexgpt.py
+++ yandexgpt.py
@@ -1,7 +1,7 @@
 from yandex_cloud_ml_sdk import YCloudML
 
 sdk = YCloudML(
-    folder_id="b1g6f7feuufg79e0jh0f", auth="t1.9euelZqOjYqSmouYyJyWnpqaj5HGxu3rnpWazJGSjpKdnszKkpSWiY-UnZ3l8_dMP0g_-e8vL1hz_N3z9wxuRT_57y8vWHP8zef1656VmpnKyYuamc6ayMyKjZDOis-Z7_zF656VmpnKyYuamc6ayMyKjZDOis-Z._Njyn-9qZe388mFnIS_LF0b0gvpUfeI35ZBKP_F6XCY6Ze2wSSM2P45K9039xloVuUh1UM2SELw1KBHpLUNqDQ"
+    folder_id="b1g6f7feuufg79e0jh0f", auth="APi"
 )
 
 model = sdk.models.completions("yandexgpt-lite", model_version="rc")
